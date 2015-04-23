7d7ce627a842dd6ec081a22ee6433090c7656eff
fdbeirao <fdblog@gmail.com>
2015-04-23 22:07:22 +0100

    Upload pluginVersion to GitHub

--- TrayRecentFiles/Properties/AssemblyInfo.cs
+++ TrayRecentFiles/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("TrayRecentFiles")]
 [assembly: AssemblyDescription("Have your recent databases available from systray")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ed0352bf-0964-4114-a9a3-9cb15dc1a41f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
